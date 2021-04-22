add18144677c6b5e7a059915e68a90afc4f04a69
Nicholas Dowell <ndowellbox@gmail.com>
2021-04-22 12:34:46 -0700

    added first components

--- grocery-app/src/app/app.module.ts
+++ grocery-app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { AdminComponent } from './admin/admin.component';
+import { EmployeeComponent } from './employee/employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    AdminComponent,
+    EmployeeComponent
   ],
   imports: [
     BrowserModule,
