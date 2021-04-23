5e2ef444fee533dedf3b65176254307b91eabc86
Nicholas Dowell <ndowellbox@gmail.com>
2021-04-22 22:10:29 -0700

    Committed initial high level functions to a separate branch hopefully!

--- grocery-app/src/app/employee/employee.component.ts
+++ grocery-app/src/app/employee/employee.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee.component.css']
 })
 export class EmployeeComponent implements OnInit {
-
+  employeeId?:number; //This will be used for login status
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  /**
+   * Checks the employee's credentials to see whether they can access employee only features
+   */
+  validateInfo(username:string, password:string){
+    //Make DB connection, 
+    //check to make sure this passwor dis associated with this username
+    //
+  }
 }
--- grocery-app/src/app/user/user.component.ts
+++ grocery-app/src/app/user/user.component.ts
@@ -6,10 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-
+  orders:Array<Object> = new Array();
   constructor() { }
 
   ngOnInit(): void {
+    //might be good to load up all the profile info here ...
   }
+  /**
+   * enerates a list of order data to be displayed in the HTML component
+   * These are stored in the "orders" member variable
+   */
+  getOrderStatus(){
+    //Make DB Connection
+    //find Orders associated with currently logged in user
+    //save those orders in a readable format
+    //display them in the HTML component
+  }
+  /**
+   * Edit Profile:
+   */
+  updateProfileInfo(newProfileInfo:any){
+    // Makes a DB connection,
+    // Get the ngForm data from HTML.
+    // It changes this user's saved info in the db
+  }
+  logOut(){
+    //redirects user back to the main page, and clears their "logged in" status
+  }
+  
+
+
+
 
 }
