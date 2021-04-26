944691906519b403a7e8588caa4d1dea576efbde
Nicholas Dowell <ndowellbox@gmail.com>
2021-04-26 11:30:19 -0700

    Added routing for main page

--- grocery-app/src/app/app-routing.module.ts
+++ grocery-app/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { UserComponent } from './user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"employee",component:EmployeeComponent},
+  {path:"user",component:UserComponent},
+  {path:"admin",component:AdminComponent}
+  //{path:"\home",component:DashboardComponent,canActivate:[MyAuthGaurd]},
+  //{path:"",redirectTo:"\login",pathMatch:"full"}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
